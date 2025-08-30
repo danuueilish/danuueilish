31c0115b0c3e2ba6af6db227d3070bb26e9754ce
danuueilish <tegardanu10@gmail.com>
2025-08-31 04:10:55 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -7,12 +7,12 @@ local base = "https://raw.githubusercontent.com/danuueilish/danuueilish/main/src
 local UI = loadstring(game:HttpGet(base.."ui_main.lua"))()
 
 -- Load fitur per file
+pcall(function() loadstring(game:HttpGet(base.."home.lua"))() end)
 pcall(function() loadstring(game:HttpGet(base.."auto_loop.lua"))() end)
 pcall(function() loadstring(game:HttpGet(base.."auto_crash.lua"))() end)
 pcall(function() loadstring(game:HttpGet(base.."sticky_notes.lua"))() end)
 pcall(function() loadstring(game:HttpGet(base.."rejoin.lua"))() end)
 pcall(function() loadstring(game:HttpGet(base.."antilag.lua"))() end)
 pcall(function() loadstring(game:HttpGet(base.."mount_atin.lua"))() end)
-pcall(function() loadstring(game:HttpGet(base.."home.lua"))() end)
 
 print("[danuu-hub] semua modul sudah di-load ✓")
